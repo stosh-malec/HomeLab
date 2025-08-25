7051a45fddc5793f5f2550a63f20c44e5ba1e04a
Stanislaw Malec <35873455+stosh-malec@users.noreply.github.com>
2025-08-24 20:53:06 -0600

    Get setup for homelab deploys

--- terraform/roots/homelab/main.tf
+++ terraform/roots/homelab/main.tf
@@ -2,11 +2,11 @@ terraform {
   required_providers {
     kubernetes = {
       source  = "hashicorp/kubernetes"
-      version = "~> 2.23"
+      version = "~> 2.38"
     }
     helm = {
       source  = "hashicorp/helm"
-      version = "~> 2.11"
+      version = "~> 2.12.1"
     }
   }
 }
@@ -23,7 +23,7 @@ provider "helm" {
   }
 }
 
-# Deploy Longhorn first as the default storage class
+# Deploy Longhorn as the default storage class
 module "longhorn" {
   source = "../../modules/longhorn"
 
@@ -35,69 +35,70 @@ module "longhorn" {
   kube_context        = var.kube_context
 }
 
-resource "kubernetes_namespace" "games" {
-  metadata {
-    name = "games"
-  }
+
+# resource "kubernetes_namespace" "games" {
+#   metadata {
+#     name = "games"
+#   }
   
-  # Ensure Longhorn is deployed first
-  depends_on = [module.longhorn]
-}
+#   # Ensure Longhorn is deployed first
+#   depends_on = [module.longhorn]
+# }
 
-# Deploy Satisfactory server using the module
-module "satisfactory_server" {
-  source = "../../modules/satisfactory"
+# # Deploy Satisfactory server using the module
+# module "satisfactory_server" {
+#   source = "../../modules/satisfactory"
 
-  name      = "satisfactory-server"
-  namespace = kubernetes_namespace.games.metadata[0].name
+#   name      = "satisfactory-server"
+#   namespace = kubernetes_namespace.games.metadata[0].name
 
-  # Use the existing PVC
-  create_persistent_volume_claim = false
-  persistent_volume_claim_name   = "satisfactory-files"
-  storage_class_name             = "longhorn-static"
-  storage_size                   = "20Gi"
+#   # Use the existing PVC
+#   create_persistent_volume_claim = false
+#   persistent_volume_claim_name   = "satisfactory-files"
+#   storage_class_name             = "longhorn-static"
+#   storage_size                   = "20Gi"
 
-  # Specify the chart path to use the custom Helm chart
-  chart_path = "satisfactory-server"
+#   # Specify the chart path to use the custom Helm chart
+#   chart_path = "satisfactory-server"
 
-  # Server configuration
-  max_players = 4
-  steambeta   = false
-  debug       = "false"
-  skip_update = "false"
+#   # Server configuration
+#   max_players = 4
+#   steambeta   = false
+#   debug       = "false"
+#   skip_update = "false"
 
-  # Don't wait for completion as we're importing an existing deployment
-  wait = true
+#   # Don't wait for completion as we're importing an existing deployment
+#   wait = true
   
-  # Ensure Longhorn is deployed first
-  depends_on = [module.longhorn]
-}
+#   # Ensure Longhorn is deployed first
+#   depends_on = [module.longhorn]
+# }
 
-# Deploy Minecraft server using the module
-module "minecraft_server" {
-  source = "../../modules/minecraft"
+# # Deploy Minecraft server using the module
+# module "minecraft_server" {
+#   source = "../../modules/minecraft"
 
-  name             = "minecraft-server"
-  namespace        = kubernetes_namespace.games.metadata[0].name
-  create_namespace = false
+#   name             = "minecraft-server"
+#   namespace        = kubernetes_namespace.games.metadata[0].name
+#   create_namespace = false
 
-  # Don't create PVC since we're using an existing Longhorn volume
-  create_persistent_volume_claim = false
-  persistent_volume_claim_name   = "minecraft-server-datadir"
-  storage_class_name             = "longhorn"
-  storage_size                   = "10Gi"
+#   # Don't create PVC since we're using an existing Longhorn volume
+#   create_persistent_volume_claim = false
+#   persistent_volume_claim_name   = "minecraft-server-datadir"
+#   storage_class_name             = "longhorn"
+#   storage_size                   = "10Gi"
 
-  # Chart configuration
-  chart_repo    = "https://itzg.github.io/minecraft-server-charts/"
-  chart_name    = "minecraft"
-  chart_version = "4.26.3"
+#   # Chart configuration
+#   chart_repo    = "https://itzg.github.io/minecraft-server-charts/"
+#   chart_name    = "minecraft"
+#   chart_version = "4.26.3"
 
-  # Use values file instead of individual settings
-  values_file_path = "${path.module}/../../../apps/mc-server/values.yaml"
+#   # Use values file instead of individual settings
+#   values_file_path = "${path.module}/../../../apps/mc-server/values.yaml"
 
-  # Wait for completion
-  wait = true
+#   # Wait for completion
+#   wait = true
   
-  # Ensure Longhorn is deployed first
-  depends_on = [module.longhorn]
-} 
+#   # Ensure Longhorn is deployed first
+#   depends_on = [module.longhorn]
+# } 
